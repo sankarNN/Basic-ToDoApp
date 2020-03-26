dd444998bb3879dc544a6a5ad5032bc1e358bea2
Kiruthika S <muthusreep@teezle.com>
2020-03-26 17:01:30 +0530

    Updates

---
+++ precache-manifest.548e7d8ed0f8ed6e048e51edcc28fc1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd86a4cca10774a24d4a6ff75284b956",
+    "url": "/sankarNN/To-do-List-using-React/index.html"
+  },
+  {
+    "revision": "d418f531ddd16798c01f",
+    "url": "/sankarNN/To-do-List-using-React/static/css/main.bcdc5c0e.chunk.css"
+  },
+  {
+    "revision": "03a7d3d9521f8089d439",
+    "url": "/sankarNN/To-do-List-using-React/static/js/2.0d4f1b4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sankarNN/To-do-List-using-React/static/js/2.0d4f1b4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d418f531ddd16798c01f",
+    "url": "/sankarNN/To-do-List-using-React/static/js/main.7b4c842d.chunk.js"
+  },
+  {
+    "revision": "44bfa95b94b1793af36b",
+    "url": "/sankarNN/To-do-List-using-React/static/js/runtime-main.1553c63b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sankarNN/To-do-List-using-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
